418799c80dcf0ef9e9df709078d05a7f038e01db
bhaveshmit <bhavesh_p@users.noreply.github.com>
2018-11-20 09:13:01 -0600

    Added new return value

--- DealzTown-SPA/src/app/app.module.ts
+++ DealzTown-SPA/src/app/app.module.ts
@@ -3,17 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-   declarations: [
-      AppComponent
-   ],
-   imports: [
-      BrowserModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- DealzTown.API/Controllers/ValuesController.cs
+++ DealzTown.API/Controllers/ValuesController.cs
@@ -23,7 +23,7 @@ namespace DealzTown.API.Controllers
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-      return new string[] { "value1", "value2" };
+      return new string[] { "value1", "value2", "value3" };
     }
 
     // GET api/values/5
