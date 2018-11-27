84cfe1410d409fbe1a22d7e3007afa6db0e60de0
bhaveshmit <bhavesh_p@users.noreply.github.com>
2018-11-27 16:56:45 -0600

    Added memberdetails,list and photos

--- DealzTown-SPA/src/app/app.module.ts
+++ DealzTown-SPA/src/app/app.module.ts
@@ -1,24 +1,35 @@
+import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgxGalleryModule } from 'ngx-gallery';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { FurnituresComponent } from './furnitures/furnitures.component';
+import { ServicesComponent } from './services/services.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { UserService } from './_services/user.service';
 import { AuthGuard } from './_guards/auth.guard';
 import { appRoutes } from './rootes';
 import { AlertifyService } from './_services/alertify.service';
 import { ErrorInteceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { RouterModule } from '@angular/router';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './member-list/member-list.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { FurnituresComponent } from './furnitures/furnitures.component';
-import { ServicesComponent } from './services/services.component';
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -30,16 +41,35 @@ import { ServicesComponent } from './services/services.component';
     ListsComponent,
     MessagesComponent,
     FurnituresComponent,
-    ServicesComponent
+    ServicesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
-  providers: [AuthService, ErrorInteceptorProvider, AlertifyService, AuthGuard],
+  providers: [
+    AuthService,
+    ErrorInteceptorProvider,
+    AlertifyService,
+    AuthGuard,
+    UserService,
+    MemberDetailResolver,
+    MemberListResolver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
