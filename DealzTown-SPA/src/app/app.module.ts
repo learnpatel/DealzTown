4281ad4ed8237d0cdab37be0cbcc94efaa48231f
bhaveshmit <bhavesh_p@users.noreply.github.com>
2018-11-27 07:01:58 -0600

    Added routes

--- DealzTown-SPA/src/app/app.module.ts
+++ DealzTown-SPA/src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { AuthGuard } from './_guards/auth.guard';
+import { appRoutes } from './rootes';
 import { AlertifyService } from './_services/alertify.service';
 import { ErrorInteceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,11 +14,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { FurnituresComponent } from './furnitures/furnitures.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot()],
-  providers: [AuthService, ErrorInteceptorProvider, AlertifyService],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    FurnituresComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [AuthService, ErrorInteceptorProvider, AlertifyService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ DealzTown-SPA/src/app/rootes.ts
@@ -0,0 +1,25 @@
+import { AuthGuard } from './_guards/auth.guard';
+import { FurnituresComponent } from './furnitures/furnitures.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+import { MessagesComponent } from './messages/messages.component';
+import { ServicesComponent } from './services/services.component';
+
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent }
+    ]
+  },
+  { path: 'lists', component: ListsComponent },
+  { path: 'furnitures', component: FurnituresComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
